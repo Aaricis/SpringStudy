658a434168f3efc7ee46daca9968bbd4ae61f5cb
Aaricis <35218694+Aaricis@users.noreply.github.com>
2021-08-26 19:12:28 +0800

    Add files via upload

---
+++ spring-study/spring-01-ioc1/src/main/java/com/kuang/service/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.kuang.service;
+
+import com.kuang.dao.UserDao;
+import com.kuang.dao.UserDaoImpl;
+import com.kuang.dao.UserDaoMysqlImpl;
+import com.kuang.dao.UserDaoOracleImpl;
+
+public class UserServiceImpl implements UserService{
+    //private UserDao userDao = new UserDaoImpl();
+    //private UserDao userDao = new UserDaoMysqlImpl();
+    //private UserDao userDao = new UserDaoOracleImpl();
+    private UserDao userDao;
+
+    //利用set方法进行动态值的注入
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public void getUser() {
+        userDao.getUser();
+    }
+}
