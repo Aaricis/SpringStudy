2a9f3ef6e197cbe64ff21bb7ac30243cb129e68e
00621429 <zhangkaige5@huawei.com>
2021-08-29 19:09:20 +0800

    Spring自动装配

---
+++ spring-05-Autowired/src/main/java/com/kuang/pojo/Dog.java
@@ -0,0 +1,13 @@
+package com.kuang.pojo;
+
+/**
+ * @program: com.kuang
+ * @description: Dog
+ * @author: zkg
+ * @create: 2021-08-26 19:54
+ **/
+public class Dog {
+    public void shout(){
+        System.out.println("miao~");
+    }
+}
