3eceedf4c9800432a7f17c25599890683a21511e
Aaricis <35218694+Aaricis@users.noreply.github.com>
2021-08-30 15:58:54 +0800

    Add files via upload

---
+++ spring-05-Autowired/src/main/java/com/kuang/pojo/Cat.java
@@ -0,0 +1,13 @@
+package com.kuang.pojo;
+
+/**
+ * @program: com.kuang
+ * @description: Cat
+ * @author: zkg
+ * @create: 2021-08-26 19:54
+ **/
+public class Cat {
+    public void shout(){
+        System.out.println("wang~");
+    }
+}
